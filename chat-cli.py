350e0f9a9890dbc69510d1c5a8e44d9e21122a3d
hafid <hfdbch@gmail.com>
2019-05-14 00:01:35 +0700

    Revert "hias menu + fix identasi + pesan logout keluar di server"
    This reverts commit f9c7cff22f680f5ca7ace65830255ac2adb348bd.

--- chat-cli.py
+++ chat-cli.py
@@ -5,106 +5,87 @@ import json
 TARGET_IP = "127.0.0.1"
 TARGET_PORT = 8889
 
-	
+
 class ChatClient:
-	def __init__(self):
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.server_address = (TARGET_IP, TARGET_PORT)
-		self.sock.connect(self.server_address)
-		self.tokenid = ""
-
-	def proses(self, cmdline):
-		j = cmdline.split(" ")
-		try:
-			command = j[0].strip()
-			if (command == 'auth'):
-				username = j[1].strip()
-				password = j[2].strip()
-				return self.login(username, password)
-			elif (command == 'send'):
-				usernameto = j[1].strip()
-				message = ""
-				for w in j[2:]:
-					message = "{} {}" . format(message, w)
-				return self.sendmessage(usernameto, message)
-			elif (command == 'inbox'):
-				return self.inbox()
-			elif (command == 'logout'):
-				return self.logout()
-			elif (command == 'menu'):
-				print '************************ Menu ************************'
-				print '*  action   		   | command  		     *'
-				print '*==========================|=========================*'
-				print '*  Login     		   |  auth username password *'
-				print '*  Logout  		   |  logout  		     *'
-				print '*  Send personal message   |  send username message  *'
-				print '*  Send personal img/file  |  send username filename *'
-				print '*  Open inbox  		   |  inbox  	             *'
-				print '*  Send group message      |  sendgroup message      *'
-				print '*  Send group img/file     |  sendgroup filename     *'
-				print '******************************************************'
-			else:
-				return "*Command not found, type `menu` to see all commands"
-		except IndexError:
-			return "*Command format wrong, type `menu` to see all commands"
-
-	def sendstring(self, string):
-		try:
-			self.sock.sendall(string)
-			receivemsg = ""
-			while True:
-				data = self.sock.recv(10)
-				if (data):
-					receivemsg = "{}{}" . format(receivemsg, data)
-					if receivemsg[-4:] == "\r\n\r\n":
-						return json.loads(receivemsg)
-		except:
-			self.sock.close()
-
-	def logout(self):
-		if (self.tokenid == ""):
-			return "Already logged out"
-		string = "logout {} \r\n" . format(self.tokenid)
-		result = self.sendstring(string)
-		if result['status'] == 'OK':
-			self.tokenid = ""
-			return "Log out successful"
-		else:
-			return "Unexpected Error(?)"
-
-	def login(self, username, password):
-		string = "auth {} {} \r\n" . format(username, password)
-		result = self.sendstring(string)
-		if result['status'] == 'OK':
-			self.tokenid = result['tokenid']
-			return "username {} logged in, token {} " .format(username, self.tokenid)
-		else:
-			return "Error, {}" . format(result['message'])
-
-	def sendmessage(self, usernameto="xxx", message="xxx"):
-		if (self.tokenid == ""):
-			return "Error, not authorized"
-		string = "send {} {} {} \r\n" . format(
-			self.tokenid, usernameto, message)
-		result = self.sendstring(string)
-		if result['status'] == 'OK':
-			return "message sent to {}" . format(usernameto)
-		else:
-			return "Error, {}" . format(result['message'])
-
-	def inbox(self):
-		if (self.tokenid == ""):
-			return "Error, not authorized"
-		string = "inbox {} \r\n" . format(self.tokenid)
-		result = self.sendstring(string)
-		if result['status'] == 'OK':
-			return "{}" . format(json.dumps(result['messages']))
-		else:
-			return "Error, {}" . format(result['message'])
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_address = (TARGET_IP,TARGET_PORT)
+        self.sock.connect(self.server_address)
+        self.tokenid=""
+    def proses(self,cmdline):
+	j=cmdline.split(" ")
+	try:
+	    command=j[0].strip()
+	    if (command=='auth'):
+		username=j[1].strip()
+		password=j[2].strip()
+		return self.login(username,password)
+	    elif (command=='send'):
+		usernameto = j[1].strip()
+                message=""
+                for w in j[2:]:
+                   message="{} {}" . format(message,w)
+		return self.sendmessage(usernameto,message)
+            elif (command=='inbox'):
+                return self.inbox()
+            elif (command=='logout'):
+                self.tokenid = ''
+                return 'Anda telah logout'
+            elif (command=='menu'):
+                print 'Login = "auth username password"'
+                print 'Logout = "logout"'
+                print 'Send personal message = "send username message"'
+                print 'Send personal Img/file = "send file username namafile"'
+                print 'Open inbox = "inbox"'
+                print 'Send group message = "send group message"'
+                return 'Send group Img/File = "send group file namafile"'
+	    else:
+		return "*Maaf, command tidak benar"
+	except IndexError:
+	    return "-Maaf, command tidak benar"
+    def sendstring(self,string):
+        try:
+            self.sock.sendall(string)
+            receivemsg = ""
+            while True:
+                data = self.sock.recv(10)
+                if (data):
+                    receivemsg = "{}{}" . format(receivemsg,data)
+                    if receivemsg[-4:]=="\r\n\r\n":
+                        return json.loads(receivemsg)
+        except:
+            self.sock.close()
+    def login(self,username,password):
+        string="auth {} {} \r\n" . format(username,password)
+        result = self.sendstring(string)
+        if result['status']=='OK':
+            self.tokenid=result['tokenid']
+            return "username {} logged in, token {} " .format(username,self.tokenid)
+        else:
+            return "Error, {}" . format(result['message'])
+    def sendmessage(self,usernameto="xxx",message="xxx"):
+        if (self.tokenid==""):
+            return "Error, not authorized"
+        string="send {} {} {} \r\n" . format(self.tokenid,usernameto,message)
+        result = self.sendstring(string)
+        if result['status']=='OK':
+            return "message sent to {}" . format(usernameto)
+        else:
+            return "Error, {}" . format(result['message'])
+    def inbox(self):
+        if (self.tokenid==""):
+            return "Error, not authorized"
+        string="inbox {} \r\n" . format(self.tokenid)
+        result = self.sendstring(string)
+        if result['status']=='OK':
+            return "{}" . format(json.dumps(result['messages']))
+        else:
+            return "Error, {}" . format(result['message'])
 
 
-if __name__ == "__main__":
-	cc = ChatClient()
-	while True:
-		cmdline = raw_input("Command {}:" . format(cc.tokenid))
-		print cc.proses(cmdline)
+if __name__=="__main__":
+    cc = ChatClient()
+    while True:
+        cmdline = raw_input("Command {}:" . format(cc.tokenid))
+        print cc.proses(cmdline)
+
